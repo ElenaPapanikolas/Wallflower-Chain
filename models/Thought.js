a989bea683a879de51af589998e42a08c45bbaf1
Elena Papanikolas <epapanikolas@hotmail.com>
2024-08-16 16:32:02 -0600

    Added Thought model

---
+++ models/Thought.js
@@ -0,0 +1,37 @@
+// Importing Schema and model objects from mongoose, importing Reaction schema, importing moment for date/time formatting
+const { Schema, model } = require('mongoose');
+const Reaction = require('./Reaction');
+const moment = require('moment');
+
+// Creating thoughtSchema
+const thoughtSchema = new Schema(
+    {
+        thoughtText: { type: String, required: true, minLength: [1], maxLength: [280] },
+        createdAt: { type: Date, default: Date.now },
+        username: { type: String, required: true },
+        reactions: [Reaction],
+    },
+    {
+        toJSON: {
+            virtuals: true, // Virtual properties on the schema will be included when schema is converted to JSON
+        },
+        id: false, // Mongoose will not automatically create an id field for each document
+    }
+);
+
+thoughtSchema.virtual('formattedCreatedAt').get(function() {
+    return moment(this.createdAt).format('MM/DD/YYYY');
+});
+
+// Virtual that gets length of reactions array to return reaction count
+thoughtSchema
+    .virtual('reactionCount')
+    .get(function () {
+        return this.reactions.length;
+    });
+
+// Creating a Thought model that will interact with the MongoDB collection called 'thought' based on the defined thoughtSchema
+const Thought = model('thought', thoughtSchema);
+    
+// Exporting Thought
+module.exports = Thought;
